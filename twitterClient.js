6c0a299cf3dcea76d7fce8818553a70a1f2dc961
Bernardo Oliveira <jb@bernardooliveira.com>
2022-09-21 22:18:39 +0100

    Return to Version 1.0 with minor changes

--- twitterClient.js
+++ twitterClient.js
@@ -1,11 +1,10 @@
 const {TwitterApi} = require('twitter-api-v2');
-const accessSecret = require('./secrets.js');
 
 const Client = new TwitterApi({
-    appKey: await accessSecret('TWITTER_API_KEY'),
-    appSecret: await accessSecret('TWITTER_API_SECRET_KEY'),
-    accessToken: await accessSecret('TWITTER_ACCESS_TOKEN'),
-    accessSecret: await accessSecret('TWITTER_ACCESS_TOKEN_SECRET'),
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_SECRET_KEY,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 const rwClient = Client.readWrite;
